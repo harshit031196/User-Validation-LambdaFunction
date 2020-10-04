6eea226c4b590116df25b603301b28006052b553
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-05 00:19:54 +0530

    Added the exception file
    This is the Validation exception file

---
+++ ValidationException.java
@@ -0,0 +1,11 @@
+
+public class ValidationException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7552478233344614142L;
+
+	public ValidationException(String message) {
+		super(message);
+	}
+}
